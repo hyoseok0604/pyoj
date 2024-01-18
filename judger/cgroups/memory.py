475779fdb6a1efa62443f8bae44851730b94a828
hyoseok0604 <janghs0604@gmail.com>
2024-01-18 12:41:17 +0900

    Remove get_max_memory_usage
    Add get_memory_usage

--- judger/cgroups/memory.py
+++ judger/cgroups/memory.py
@@ -4,13 +4,25 @@ from judger.cgroups.base import BaseCgroups
 
 
 class MemoryCgroups(BaseCgroups):
+    def __init__(self, path: str, name: str) -> None:
+        super().__init__(path, name, "memory")
+
     def set_memory_limit(self, memory_limit: int):
         """Set memory limit in bytes"""
         self._write("memory.limit_in_bytes", str(memory_limit))
         self._write("memory.memsw.limit_in_bytes", str(memory_limit))
 
-    def get_max_memory_usage(self) -> int:
-        return int(self._read("memory.max_usage_in_bytes")[0]) // 1024
+    def get_memory_usage(self) -> int:
+        usage = 0
+        for line in self._read("memory.stat"):
+            name, value = line.strip().split(" ")
+
+            if name in ["rss", "mapped_file", "cache"]:
+                usage += int(value)
+        return usage
+
+    def get_stat(self) -> str:
+        return "\n".join(self._read("memory.stat"))
 
     def get_oom_kill_count(self) -> int:
         lines = self._read("memory.oom_control")
