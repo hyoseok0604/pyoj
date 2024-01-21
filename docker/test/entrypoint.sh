68b738b9ed75a294f2ba317bf5f14d19e719a860
hyoseok0604 <janghs0604@gmail.com>
2024-01-22 02:48:38 +0900

    Update test entrypoint script

--- docker/test/entrypoint.sh
+++ docker/test/entrypoint.sh
@@ -1,30 +1,9 @@
 #!/bin/bash
 
-# https://www.kernel.org/doc/Documentation/cgroup-v1/cgroups.txt
+# Mount cgroup v2
+echo "+memory +cpu" > /sys/fs/cgroup/cgroup.subtree_control
+chown -hR test:test /sys/fs/cgroup
 
-# Mount root cgroups
-if [ ! -d "/cgroups" ]; then
-  mkdir /cgroups
-fi
+chown test:test /src
 
-mount -t tmpfs cgroup_root /cgroups
-
-# Create directory and setting for cgroup memory
-if [ ! -d "/cgroups/memory" ]; then
-  mkdir /cgroups/memory
-fi
-
-mount -t cgroup -o memory memory /cgroups/memory
-chown -hR test:test /cgroups/memory
-chmod -R +w /cgroups/memory
-
-# Create directory and setting for cgroup cpuacct
-if [ ! -d "/cgroups/cpuacct" ]; then
-  mkdir /cgroups/cpuacct
-fi
-
-mount -t cgroup -o cpuacct cpuacct /cgroups/cpuacct
-chown -hR test:test /cgroups/cpuacct
-chmod -R +w /cgroups/cpuacct
-
-poetry run pytest
+su test --command "poetry run pytest"
