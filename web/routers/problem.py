09d5b2e9245dd17f08c6aa68ad7532af4c7cfca6
hyoseok0604 <janghs0604@gmail.com>
2024-02-12 23:13:24 +0900

    query는  pydantic을 지원하지 않아서 수정

--- web/routers/problem.py
+++ web/routers/problem.py
@@ -36,7 +36,23 @@ async def get_problem_api(id: int, service: ProblemService):
 
 
 @api_router.get("", response_model=list[GetProblemsResponseSchema])
-async def get_problems_api(service: ProblemService, schema: GetProblemsRequestSchema):
+async def get_problems_api(
+    service: ProblemService,
+    title: Optional[str] = Query(default=None),
+    creator: Optional[str] = Query(default=None),
+    is_public: bool = Query(default=False),
+    page: int = Query(default=1, ge=1),
+    count: int = Query(default=20, ge=1, le=100),
+    sort: Literal["asc", "desc"] = Query(default="desc"),
+):
+    schema = GetProblemsRequestSchema(
+        title=title,
+        creator=creator,
+        is_public=is_public,
+        count=count,
+        page=page,
+        sort=sort,
+    )
     problems = await service.get_problems(schema)
 
     return problems
