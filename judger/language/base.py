1554cc37b7c866c01c090df2bdb0aad7652451bf
hyoseok0604 <janghs0604@gmail.com>
2024-02-26 21:40:12 +0900

    언어에 display_name 옵션 추가

--- judger/language/base.py
+++ judger/language/base.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 
 @dataclass(kw_only=True, frozen=True)
 class Language:
+    display_name: str
     filename: str
     compile_command: str
     execute_command: str
