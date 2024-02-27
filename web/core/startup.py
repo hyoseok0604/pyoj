10870efb78a168cf1e6c21689d1fddedec8d4eba
hyoseok0604 <janghs0604@gmail.com>
2024-02-27 18:36:57 +0900

    시작 과정에서 쿼리 로깅 비활성화

--- web/core/startup.py
+++ web/core/startup.py
@@ -1,46 +1,68 @@
-from sqlalchemy import select
+from sqlalchemy import select, update
 from sqlalchemy.orm import Session
 
 from judger.language import languages as judger_languages
 from web.core.database import async_session
 from web.core.migration import migration
+from web.logger import (
+    DisableSqlalchemyLogger,
+    _log,
+)
 from web.models.base import BaseModel
 from web.models.language import Language
 
 
 async def startup_migration():
-    async with async_session() as session:
+    with DisableSqlalchemyLogger():
+        async with async_session() as session:
 
-        def wrapped_migration(session: Session):
-            migration(session.connection(), BaseModel.metadata)
+            def wrapped_migration(session: Session):
+                migration(session.connection(), BaseModel.metadata)
 
-        await session.run_sync(wrapped_migration)
-        await session.commit()
+            await session.run_sync(wrapped_migration)
+            await session.commit()
 
 
 async def startup_language_migration():
-    async with async_session() as session:
-        for judger_language in judger_languages:
-            stmt = select(Language).where(
-                Language.display_name == judger_language.display_name
+    with DisableSqlalchemyLogger():
+        async with async_session() as session:
+            _log.info(f"{len(judger_languages)} languages detected.")
+
+            for judger_language in judger_languages:
+                stmt = select(Language).where(
+                    Language.display_name == judger_language.display_name
+                )
+
+                language = await session.scalar(stmt)
+                is_new = language is None
+
+                if language is None:
+                    language = Language()
+
+                language.display_name = judger_language.display_name
+                language.filename = judger_language.filename
+                language.compile_command = judger_language.compile_command
+                language.execute_command = judger_language.execute_command
+
+                if is_new:
+                    session.add(language)
+                    _log.info(f"Create language {language.display_name}.")
+                else:
+                    await session.merge(language)
+                    _log.info(f"Update language {language.display_name}.")
+
+            stmt = (
+                update(Language)
+                .where(
+                    Language.display_name.not_in(
+                        v.display_name for v in judger_languages
+                    )
+                )
+                .values({Language.is_enabled: False})
             )
 
-            language = await session.scalar(stmt)
-            is_new = language is None
-
-            if language is None:
-                language = Language()
-
-            language.display_name = judger_language.display_name
-            language.filename = judger_language.filename
-            language.compile_command = judger_language.compile_command
-            language.execute_command = judger_language.execute_command
-
-            if is_new:
-                session.add(language)
-            else:
-                await session.merge(language)
-
+            result = await session.execute(stmt)
+            _log.info(f"Disable {result.rowcount} languages.")
             await session.commit()
 
 
--- web/logger.py
+++ web/logger.py
@@ -1,3 +1,23 @@
 import logging
+from contextlib import AbstractContextManager
+from types import TracebackType
 
 _log = logging.getLogger("web")
+
+
+class DisableSqlalchemyLogger(AbstractContextManager):
+    def __init__(self) -> None:
+        self.logger = logging.getLogger("sqlalchemy.engine.Engine")
+
+    def __enter__(self):
+        self.handlers = self.logger.handlers
+        self.logger.handlers = []
+        return self
+
+    def __exit__(
+        self,
+        __exc_type: type[BaseException] | None,
+        __exc_value: BaseException | None,
+        __traceback: TracebackType | None,
+    ) -> bool | None:
+        self.logger.handlers = self.handlers
