abbc4c253d288127df27304aa300ffdd9330dcb4
hyoseok0604 <janghs0604@gmail.com>
2024-03-05 20:23:16 +0900

    테스트를 위한 값들 추가

--- web/core/migration.py
+++ web/core/migration.py
@@ -10,9 +10,24 @@ from alembic_postgresql_enum.operations import (
     DropEnumOp,
     SyncEnumValuesOp,
 )
-from sqlalchemy import Connection, MetaData
+from sqlalchemy import Connection, MetaData, create_engine
+from sqlalchemy.orm import Session
 
+from web.core.settings import settings
 from web.logger import DisableSqlalchemyLogger, _log
+from web.models import (
+    Language,
+    LanguageSystemcallCountLimit,
+    Problem,
+    SessionUser,
+    Submission,
+    SubmissionTestcaseResult,
+    Systemcall,
+    SystemcallCount,
+    SystemcallGroup,
+    Testcase,
+    User,
+)
 
 EnumOp = CreateEnumOp | DropEnumOp | SyncEnumValuesOp
 
@@ -96,3 +111,80 @@ def _run_enum_operations(
 
 def _nothing(rev, context):
     return []
+
+
+if __name__ == "__main__":
+    engine = create_engine(str(settings.postgres_uri))
+    connection = Connection(engine=engine)
+    session = Session(connection)
+
+    user = User()
+    user.username = "test"
+    user.password = "test"
+    session.add(user)
+
+    session_user = SessionUser()
+    session_user.session_key = "key"
+    session_user.user = user
+    session.add(session_user)
+
+    problem = Problem()
+    problem.title = "title"
+    problem.description = ""
+    problem.input_description = ""
+    problem.output_description = ""
+    problem.limit_description = ""
+    problem.creator = user
+    session.add(problem)
+
+    testcase = Testcase()
+    testcase.input_preview = "in"
+    testcase.output_preview = "out"
+    testcase.input_size = 1
+    testcase.output_size = 1
+    testcase.original_input_filename = "in"
+    testcase.original_output_filename = "out"
+    testcase.problem = problem
+    session.add(testcase)
+
+    language = Language()
+    language.display_name = "lang"
+    language.filename = "a.a"
+    language.compile_command = "command"
+    language.execute_command = "command"
+    session.add(language)
+
+    systemcall_group = SystemcallGroup()
+    session.add(systemcall_group)
+
+    systemcall = Systemcall()
+    systemcall.name = "systemcall"
+    systemcall.number = 1
+    systemcall.systemcall_group = systemcall_group
+    session.add(systemcall)
+
+    language_systemcall_count_limit = LanguageSystemcallCountLimit()
+    language_systemcall_count_limit.language = language
+    language_systemcall_count_limit.systemcall = systemcall
+    language_systemcall_count_limit.count = 11
+    session.add(language_systemcall_count_limit)
+
+    submission = Submission()
+    submission.code = "code"
+    submission.creator = user
+    submission.language = language
+    submission.problem = problem
+    session.add(submission)
+
+    submission_testcase_result = SubmissionTestcaseResult()
+    submission_testcase_result.submission = submission
+    submission_testcase_result.testcase = testcase
+    session.add(submission_testcase_result)
+
+    systemcall_count = SystemcallCount()
+    systemcall_count.count = 11
+    systemcall_count.systemcall = systemcall
+    systemcall_count.submission_result = submission_testcase_result
+    session.add(systemcall_count)
+
+    session.commit()
